425f070c129139c56e3a1b83aaa2672a4780547e
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-03-18 15:26:20 +0000

    Add verify jwt and publish version

--- handlers.go
+++ handlers.go
@@ -172,3 +172,30 @@ func verifyPassword(password, expectedHash []byte) bool {
 
 	return true
 }
+
+func VerifyJWT(w *http.ResponseWriter, r *http.Request) (err error, username string) {
+	c, err := r.Cookie(TokenCookieName)
+
+	if err != nil {
+		utils.HandleCookieError(w, RefreshName, err)
+		return err, ""
+	}
+
+	tknStr := c.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		utils.HandleJWTVerifyingError(w, RefreshName, err)
+		return err, ""
+	}
+
+	if !tkn.Valid && time.Unix(claims.ExpiresAt, 0).Unix() < time.Now().Unix() {
+		(*w).WriteHeader(http.StatusUnauthorized)
+		return err, ""
+	}
+
+	return nil, claims.Username
+}
